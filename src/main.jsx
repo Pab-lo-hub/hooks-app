41a8a080637bf3d64c2257a9d7e94e6b8a4a8171
Pab-lo-hub <salaberry.pablo@gmail.com>
2023-04-10 15:18:53 -0300

    Installing react router last version

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 
 
 // import { HooksApp } from './HooksApp';
@@ -18,9 +19,12 @@ import ReactDOM from 'react-dom/client';
 import { TodoApp } from './08-useReducer/TodoApp';
 
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <TodoApp />
-  // </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <TodoApp />
+    </React.StrictMode>
+  </BrowserRouter>
 )
